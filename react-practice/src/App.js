ca87885781331b5d7baaa5b0a9b06f787828c7f4
BumgeunSong <isp1195@gmail.com>
2024-03-02 21:41:43 +0900

    버튼 추가

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,9 +1,12 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <h1>Welcome to Eddy's App</h1>
+          <Button text="let's go"/>
         </div>
       </header>
     </div>
