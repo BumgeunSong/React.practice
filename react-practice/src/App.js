8edfdbd125d7c063a840eb8a8728ca79b18c7c02
BumgeunSong <isp1195@gmail.com>
2024-03-02 21:53:44 +0900

    앱 컴포넌트에 적용할 스타일 추가

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,12 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <h1>Welcome to Eddy's App</h1>
+          <h1 className={styles.title}>Welcome to Eddy's App</h1>
           <Button text="let's go"/>
         </div>
       </header>
