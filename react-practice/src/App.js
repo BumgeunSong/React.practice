74d62de3f3749713fdb847932eab4782659a9f2f
BumgeunSong <isp1195@gmail.com>
2024-03-03 21:24:30 +0900

    useState를 사용해 counter 기능 다시 만들기

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,13 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <h1 className={styles.title}>Welcome to Eddy's App</h1>
-          <Button text="let's go"/>
+          <p>{counter}</p>
+          <Button text="Count up" onClick={onClick}/>
         </div>
       </header>
     </div>
