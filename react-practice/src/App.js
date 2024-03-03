16a33cfd19c5a96ba86b82d1c83a57e0b29c4d6c
BumgeunSong <isp1195@gmail.com>
2024-03-03 22:05:15 +0900

    form을 추가하고 submit하면 toDo를 초기화

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -5,10 +5,18 @@ import { useState, useEffect } from "react";
 function App() {
   const [toDo, setToDo] = useState("")  
   const onChange = (event) => setToDo(event.target.value);
-  console.log(toDo);
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (toDo == "") { return }
+    setToDo("")
+  }
+
   return (
     <div className="App">
+      <form onSubmit={onSubmit}>
       <input value={toDo} type="text" placeholder="투두를 입력하세요" onChange={onChange}/>
+      <button>투두 추가하기</button>
+      </form>
     </div>
   );
 }
