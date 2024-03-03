d85a0d599bbd7849f50ece3d695e3ce347c4036f
BumgeunSong <isp1195@gmail.com>
2024-03-03 22:02:11 +0900

    To-do를 받기 위한 간단한 input 만들기

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -3,37 +3,12 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value)
-
-  const callAPI = () => {
-    console.log("Call THE API..")
-  }
-
-  const searchKeyword = () => {
-    if (keyword.length > 5) {
-      console.log("Search for " + keyword)
-    }
-  }
-
-  const showCounter = () => {
-      console.log("Counter: " + counter)
-  }
-
-  useEffect(searchKeyword, [keyword]) // keyword state가 변경될 때만 searchKeyword를 실행한다.
-  useEffect(showCounter, [counter])
+  const [toDo, setToDo] = useState("")  
+  const onChange = (event) => setToDo(event.target.value);
+  console.log(toDo);
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <h1 className={styles.title}>Welcome to Eddy's App</h1>
-          <input value={keyword} type="text" placeholder="Search here" onChange={onChange}></input>
-          <p>{counter}</p>
-          <Button text="Count up" onClick={onClick}/>
-        </div>
-      </header>
+      <input value={toDo} type="text" placeholder="투두를 입력하세요" onChange={onChange}/>
     </div>
   );
 }
