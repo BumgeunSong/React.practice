d3c47307f40f1d5cd89eb9645380465ed898ca21
BumgeunSong <isp1195@gmail.com>
2024-03-04 10:53:59 +0900

    Tic-tac-toe 게임 만들어보기

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TicTacToe from './Tic-tac-toe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App />
+    <TicTacToe />
   // </React.StrictMode>
 );
