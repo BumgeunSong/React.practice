38481e669bdc2a885be591df64ef7a61fecf012b
BumgeunSong <isp1195@gmail.com>
2024-03-07 11:25:36 +0900

    createContext를 사용해서 global 상태값 만들기

--- react-practice/src/Tic-tac-toe.js
+++ react-practice/src/Tic-tac-toe.js
@@ -2,21 +2,26 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import styles from './Tic-tac-toe.module.css';
 import { useState, useEffect } from "react";
+import { createContext, useContext } from 'react';
 
 import App from './App';
 
 function TicTacToe() {
     const defaultBoardModel = Array(3).fill(Array(3).fill("X"))
     const [boardModel, setBoardModel] = useState(defaultBoardModel)
+    const BoardContext = createContext(defaultBoardModel)
 
     return (
         <div className="TicTacToe">
-            <Board boardModel={boardModel} />
+            <BoardContext.Provider boardModel={boardModel}>
+                <Board boardModel={boardModel} />
+            </BoardContext.Provider>
         </div>
     )
 }
 
 function Board({ boardModel }) {
+    const boardModelFromContext = useContext(BoardContext)
     return (
         <div className="Board">
             {
