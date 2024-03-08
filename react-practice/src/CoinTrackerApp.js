e3c7c6d4897248cd8641ecefae0b9ed35aa65a58
BumgeunSong <isp1195@gmail.com>
2024-03-08 12:56:15 +0900

    로딩이 끝났을 때만 Coin 리스트가 보이도록

--- react-practice/src/CoinTrackerApp.js
+++ react-practice/src/CoinTrackerApp.js
@@ -3,32 +3,35 @@ import styles from "./CoinTrackerApp.module.css";
 import { useState, useEffect } from "react";
 
 function CoinTrackerApp() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([])
 
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-    .then((response) => response.json())
-    .then((json) => {
-        setCoins(json)
-        setLoading(false)
-    })
-  }, [])
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json)
+                setLoading(false)
+            })
+    }, [])
 
-  return (
-    <div className="App">
-      <h1>오늘의 비트코인 가격</h1>
-      {loading ? <p>loading...</p> : null}
-      { coins.map((coin) => <CoinView key={coin.key} coin={coin}/>) }
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Coins & Price</h1>
+            {loading ? (
+                <p>loading...</p>
+            ) : (
+                coins.map((coin) => <CoinView key={coin.key} coin={coin} />)
+            )}
+        </div>
+    );
 }
 
 function CoinView({ coin }) {
     return (
         <div>
             <h3>
-               {coin.name}
+                {coin.name}
             </h3>
             <p>
                 {coin.quotes.USD.price.toFixed(2)} <strong>USD</strong>
