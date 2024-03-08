607fb548ec2a648951ac5ee029aa20fdccc5f1fc
BumgeunSong <isp1195@gmail.com>
2024-03-08 12:36:45 +0900

    CoinTracker 기본 앱 생성하고 index로 교체

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CoinTrackerApp from './CoinTrackerApp';
 import TicTacToe from './Tic-tac-toe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <TicTacToe />
+      <CoinTrackerApp />
   // </React.StrictMode>
 );
