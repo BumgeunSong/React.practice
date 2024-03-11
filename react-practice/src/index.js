35d86cdc621d6d53561dbe8acc71b7c14d9d1a84
BumgeunSong <isp1195@gmail.com>
2024-03-11 20:34:57 +0900

    react-router 임포트하고 홈 진입점에 추가

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -4,10 +4,32 @@ import App from './App';
 import CoinTrackerApp from './CoinTrackerApp';
 import TicTacToe from './Tic-tac-toe';
 import MovieApp from './MovieApp';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/movies",
+    element: <MovieApp />,
+  },
+  {
+    path: "/movies/detail",
+    element: <Detail />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-      <MovieApp></MovieApp>
+  <RouterProvider router={router} />
   // </React.StrictMode>
 );
