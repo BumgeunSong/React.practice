b04bfc321ad3ea6b0284ff89d19f355b599e55bf
BumgeunSong <isp1195@gmail.com>
2024-03-11 11:09:45 +0900

    영화 정보 가져오는 함수를 async로 추출

--- react-practice/src/MovieApp.js
+++ react-practice/src/MovieApp.js
@@ -4,15 +4,14 @@ function MovieApp() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState({})
 
-    useEffect(() => {
-        fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
-            .then((response) => response.json())
-            .then((json) => {
-                setMovies(json.data.movies)
-                setLoading(false)
-                console.log(json.data.movies)
-            })
-    }, [])
+    async function getMovies() {
+        let response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+        let json = await response.json()
+        setMovies(json.data.movies)
+        setLoading(false)
+    }
+
+    useEffect(getMovies, []);
 
     return (
         <div className="App">
