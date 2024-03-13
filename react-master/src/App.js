3b905e0ea4f849541ecf4b9d2a846163283c77be
BumgeunSong <isp1195@gmail.com>
2024-03-13 11:42:05 +0900

    styled 컴포넌트로 기본 div 만들어보기

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+
 function App() {
+
+  const Parent = styled.div`
+    display: flex;
+  `
+
+  const Box = styled.div`
+    background-color: teal;
+    width: 100;
+    height: 100;
+  `
+
   return (
-    <div>
-      <h1>Booya</h1>
-    </div>
+    <Parent>
+      <Box />
+      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+    </Parent>
   );
 }
 
