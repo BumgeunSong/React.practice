4aefa26279dfaea2d589914d85c67a8108955ac7
BumgeunSong <isp1195@gmail.com>
2024-03-17 17:01:19 +0900

    타입을 가미해서 컴포넌트에 eventHandler 붙이기

--- react-master-ts/src/App.tsx
+++ react-master-ts/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function App() {
+  const [value, setValue] = useState("")
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setValue(value)
+  }
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(value)
+  }
   return (
     <div>
-      <form>
-        <input type="text" placeholder='username'>
+      <form onSubmit={onSubmit}>
+        <input value={value} type="text" placeholder='username' onChange={onChange}>
         </input>
+        <button>Login</button>
       </form>
     </div>
   );
