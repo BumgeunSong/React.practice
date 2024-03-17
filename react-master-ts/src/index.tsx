a18a1ea27a0028397ff0e07aecfdafc159a28820
BumgeunSong <isp1195@gmail.com>
2024-03-18 07:06:48 +0900

    ThemeProvider 삭제

--- react-master-ts/src/index.tsx
+++ react-master-ts/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,8 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    
     <App />
-    
   </React.StrictMode>
 );
