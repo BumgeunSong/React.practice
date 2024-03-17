e0283f2ee020d76b0fddb0d30115658cf4ecdaac
BumgeunSong <isp1195@gmail.com>
2024-03-17 16:50:59 +0900

    Circle을 삭제하고 text input 생성

--- react-master-ts/src/App.tsx
+++ react-master-ts/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import Circle from './Circle';
 
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" borderColor="red" />
-      <Circle bgColor="tomato" />
+      <form>
+        <input type="text" placeholder='username'>
+        </input>
+      </form>
     </div>
   );
 }
