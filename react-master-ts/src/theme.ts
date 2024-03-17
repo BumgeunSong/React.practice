a39fabfdd5b0c9af39e7a5a7eedb6adef936b12d
BumgeunSong <isp1195@gmail.com>
2024-03-18 07:07:13 +0900

    Typescript 사용해서 theme 선언하기

---
+++ react-master-ts/src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+    borderRadius: '5px',
+    colors: {
+      main: 'cyan',
+      secondary: 'magenta'
+    }
+  }
