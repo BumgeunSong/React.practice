e78cd2042087cfe6f58db341a9aedc850337ce1f
BumgeunSong <isp1195@gmail.com>
2024-03-17 16:26:17 +0900

    Circle, Circle의 style에 들어갈 prop을 ts interface로 정의

--- react-master-ts/src/Circle.tsx
+++ react-master-ts/src/Circle.tsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 
+interface ContainerProps {
+    bgColor: string;
+    borderMaker: (radius: number) => string
+}
+
 const Container = styled.div<ContainerProps>`
     width: 100px;
     height: 100px;
+    border-radius: ${props => props.borderMaker(20)};
+    background-color: ${props => props.bgColor};
 `;
+
+interface CircleProps {
+    bgColor: string
+}
 
 function Circle({ bgColor }: CircleProps) {
     return <Container bgColor={bgColor} borderMaker={(radius) => `${radius}px`}/>;
 }
+
 export default Circle
