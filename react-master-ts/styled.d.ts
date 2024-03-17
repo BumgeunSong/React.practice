ff34c97c07f157494459ec5e5f4ed76f55c36355
BumgeunSong <isp1195@gmail.com>
2024-03-18 07:09:28 +0900

    테마 선언으로 라이트, 다크 모드 만들기

--- react-master-ts/styled.d.ts
+++ react-master-ts/styled.d.ts
@@ -5,12 +5,7 @@ import 'styled-components';
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
-    borderRadius: string;
-
-
-    colors: {
-      main: string;
-      secondary: string;
-    };
+    textColor: string;
+    bgColor: string;
   }
 }
