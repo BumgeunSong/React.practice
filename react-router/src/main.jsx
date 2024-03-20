2975488e808ae874786b0f9856f33db46ef8433b
BumgeunSong <isp1195@gmail.com>
2024-03-20 11:26:06 +0900

    기본 라우터 세팅해주기

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><h1>Less but better</h1></div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
