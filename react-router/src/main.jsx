0cbc8e87a0b45b9a5cbfd99424a51fd732f3b3a2
BumgeunSong <isp1195@gmail.com>
2024-03-20 11:26:34 +0900

    루트에 보여줄 화면 추가하고 라우터에 연결

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/root'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div><h1>Less but better</h1></div>
+    element: <Root />
   }
 ]);
 
