f01a0075e9a23d1002caf8056a38141c9388a160
BumgeunSong <isp1195@gmail.com>
2024-03-23 23:43:06 +0900

    불러온 연락처 데이터를 side바에 UI표시

--- react-router/src/routes/root.jsx
+++ react-router/src/routes/root.jsx
@@ -36,14 +36,30 @@ export default function Root() {
             </form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <Link to={`contacts/1`}>Your name</Link>
-              </li>
-              <li>
-                <Link to={`contacts/2`}>Your Firend</Link>
-              </li>
-            </ul>
+            {
+              contacts.length ? (
+                <ul>
+                  {contacts.map((contact) => (
+                    <li key={contact.id}>
+                      <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                        </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>
+                  <i>연락처 없음</i>
+                </p>
+              )
+            }
           </nav>
         </div>
         <div id="detail">
