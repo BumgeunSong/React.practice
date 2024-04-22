bff6e65c6218a13b91e2f704769a8a3d34e30da3
BumgeunSong <isp1195@gmail.com>
2024-04-22 10:08:42 +0900

    Coin 목록과 Coin 상세 페이지 1개씩 만들기

---
+++ react-master-ts/src/routes/Coin.tsx
@@ -0,0 +1,3 @@
+export default function Coin() {
+    return <h1>I am Coin</h1>
+}
---
+++ react-master-ts/src/routes/Coins.tsx
@@ -0,0 +1,7 @@
+import Coin from "./Coin";
+
+export default function Coins() {
+    return <>
+        <h1>These are coins</h1>
+    </>
+}
