f0604c93693977c9ba73672e8a37de1daa8dd75c
BumgeunSong <isp1195@gmail.com>
2024-04-23 10:27:03 +0900

    Header와 Sample을 사용한 Coin List 추가

--- react-master-ts/src/routes/Coins.tsx
+++ react-master-ts/src/routes/Coins.tsx
@@ -2,11 +2,64 @@ import styled from "styled-components";
 import Coin from "./Coin";
 
 const Title = styled.h1`
-    color: ${props => props.theme.primary }
+    color: ${props => props.theme.primary}
 `
 
+const Container = styled.div``
+const Header = styled.header``
+const CoinList = styled.ul``
+const CoinItem = styled.li``
+
+const coinSample =
+    [
+        {
+            "id": "tnb-time-new-bank",
+            "name": "Time New Bank",
+            "symbol": "TNB",
+            "rank": 0,
+            "is_new": false,
+            "is_active": false,
+            "type": "token"
+        },
+        {
+            "id": "cat-catcoin-inu",
+            "name": "CatCoin Inu",
+            "symbol": "CAT",
+            "rank": 0,
+            "is_new": false,
+            "is_active": false,
+            "type": "token"
+        },
+        {
+            "id": "xpa-xpa",
+            "name": "XPA",
+            "symbol": "XPA",
+            "rank": 0,
+            "is_new": false,
+            "is_active": false,
+            "type": "token"
+        },
+        {
+            "id": "doge-dogearm",
+            "name": "DOGEARM",
+            "symbol": "$DOGE",
+            "rank": 0,
+            "is_new": false,
+            "is_active": false,
+            "type": "token"
+        }
+    ];
+
 function Coins() {
-    return <Title>코인!</Title>
+    return <Container>
+        <Header>
+            <Title>코인 목록</Title>
+        </Header>
+        <CoinList>
+            {coinSample.map(coin => <CoinItem key={coin.id}>name: {coin.name}</CoinItem>)}
+        </CoinList>
+    </Container>
 }
+
 
 export default Coins;
