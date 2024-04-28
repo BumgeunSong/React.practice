c7a1952d6851c1235713ba3633efa29e742b6abf
BumgeunSong <isp1195@gmail.com>
2024-04-28 22:00:44 +0900

    Coin 상세 데이터 적용해서 가져오기

--- react-master-ts/src/routes/Coin.tsx
+++ react-master-ts/src/routes/Coin.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom"
+import CoinInfoInterface from "./CoinInfoInterface";
 
 interface RouterState {
     name: string;
@@ -8,10 +10,22 @@ export default function Coin() {
     const { coinId } = useParams()
     const location = useLocation()
     const state = location.state as RouterState
+
+    const [coinInfo, setCoinInfo] = useState<CoinInfoInterface>()
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`);
+            const json = await response.json();
+            setCoinInfo(json)
+        })();
+    }, [])
+
+
     return (
         <>
             {state ? (
-                <h1>이것은 {state.name} 코인</h1>
+                <><h1>이것은 {state.name} 코인</h1><p>name: {coinInfo?.name}</p><p>Description: {coinInfo?.description}</p><img src={`${coinInfo?.whitepaper.thumbnail}`} /><p>Description: {coinInfo?.whitepaper.link}</p></>
             ) : (
                 <h1>Loading...</h1>
             )}
