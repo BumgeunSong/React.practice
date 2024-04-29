19ea347d9dcfbc7b0ef8334b16e374e0a8a99210
BumgeunSong <isp1195@gmail.com>
2024-04-29 10:49:28 +0900

    Coin 가격을 가져와서 리스트로 표시하기

--- react-master-ts/src/routes/Coin.tsx
+++ react-master-ts/src/routes/Coin.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom"
 import CoinInfoInterface from "./CoinInfoInterface";
 import styled from "styled-components";
+import { CoinPrice } from "./CoinPriceInterface";
 
 interface RouterState {
     name: string;
@@ -29,13 +30,23 @@ export default function Coin() {
     const state = location.state as RouterState
 
     const [coinInfo, setCoinInfo] = useState<CoinInfoInterface>()
+    const [coinPrices, setCoinPrices] = useState<[CoinPrice]>()
 
     useEffect(() => {
-        (async () => {
+        const fetchCoinInfo = async () => {
             const response = await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`);
             const json = await response.json();
             setCoinInfo(json)
-        })();
+        }
+
+        const fetchCoinPrices = async () => {
+            const reponse = await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}/markets?quotes=KRW`);
+            const json = await reponse.json();
+            setCoinPrices(json.slice(0, 20))
+        }
+
+        fetchCoinInfo()
+        fetchCoinPrices()
     }, [])
 
     return (
@@ -44,9 +55,15 @@ export default function Coin() {
                 <>
                 <CoinTitle>이것은 {state.name} 코인</CoinTitle>
                 <p>name: {coinInfo?.name}</p>
-                <CoinDescription>Description\n{coinInfo?.description}</CoinDescription>
+                <CoinDescription>Description - {coinInfo?.description}</CoinDescription>
+                <h2>코인 가격</h2>
+                {coinPrices?.map((coinPrice) => 
+                    <CoinPriceItem key={coinPrice.exchange_id} exchange_name={coinPrice.exchange_name} price={coinPrice.quotes.KRW.price} />
+                )
+
+                }
                 <CoinImage src={`${coinInfo?.whitepaper.thumbnail}`} />
-                <p>Description: {coinInfo?.whitepaper.link}</p>
+                <a href={`${coinInfo?.whitepaper.link}`}>Whitepaper</a>
                 </>
             ) : (
                 <h1>Loading...</h1>
@@ -54,3 +71,22 @@ export default function Coin() {
         </CoinContainer>
     );
 }
+
+interface CoinPriceProps {
+    price: number;
+    exchange_name: string;
+}
+
+function CoinPriceItem(props: CoinPriceProps) {
+    return (
+        <CoinItem>
+            {props.exchange_name}: {props.price.toFixed(2)} KRW
+        </CoinItem>
+    )
+}
+
+const CoinItem = styled.li`
+    margin-top: 16px;
+    margin-bottom: 16px;
+    border-radius: 16px;
+`
