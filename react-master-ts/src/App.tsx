565480e1ceb4fa0b4f42e9a36707c10fc902eeb4
BumgeunSong <isp1195@gmail.com>
2024-05-02 17:56:17 +0900

    react query 설치하기

--- react-master-ts/src/App.tsx
+++ react-master-ts/src/App.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
-import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from './theme';
 import Router from './routes/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <ThemeProvider theme={darkTheme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- react-master-ts/src/index.tsx
+++ react-master-ts/src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Router from './routes/Router';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
