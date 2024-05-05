68167e0b31fd3054fa3d24f89dae9ec9e5e0792b
BumgeunSong <isp1195@gmail.com>
2024-05-05 22:29:25 +0900

    History 데이터 가져와서 띄우기

--- react-master-ts/src/routes/Chart.tsx
+++ react-master-ts/src/routes/Chart.tsx
@@ -1,5 +1,37 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../Api";
+import styled from "styled-components";
+
+interface ICoinPriceHistory {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
+
 function Chart() {
-    return <h1>Chart</h1>
+    const { coinId } = useParams()
+    const { isLoading, data } = useQuery<ICoinPriceHistory[]>(["history", coinId], () => fetchCoinHistory(coinId))
+    return (
+        <>
+            <h1>Chart</h1>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                data?.map((priceHistory) => (
+                    <div>
+                        <p>Open Price: {priceHistory.open}</p>
+                        <p>Close Price: {priceHistory.open}</p>
+                    </div>
+                ))
+            )}
+        </>
+    );
 }
 
 export default Chart;
