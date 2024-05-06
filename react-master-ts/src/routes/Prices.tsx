2f8e42019aab1fcebef1f87271b2c97ce0890401
BumgeunSong <isp1195@gmail.com>
2024-05-06 12:59:54 +0900

    배운 것 주석 추가

--- react-master-ts/src/routes/Prices.tsx
+++ react-master-ts/src/routes/Prices.tsx
@@ -28,4 +28,10 @@ function Prices() {
     )
 }
 
+/*
+useQuery vs useLoaderData
+- useQuery: 다양한 상황에 사용. 클라이언트 사이드에서 캐싱, 백그라운드 업데이트, 쿼리 무효화 등 API 관련 기능 제공
+- useLoaderData: 특정 URL의 컴포넌트를 렌더링하기 위한 데이터가 필요할 때. 서버 사이드 렌더링, 정적 사이트 생성(SSG)가 필요한 경우
+*/
+
 export default Prices;
