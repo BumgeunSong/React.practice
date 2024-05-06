9678b25937471ca741180a3fc0729102a9511b12
BumgeunSong <isp1195@gmail.com>
2024-05-06 15:23:00 +0900

    불필요한 변수 삭제

--- react-master-ts/src/App.tsx
+++ react-master-ts/src/App.tsx
@@ -3,16 +3,14 @@ import { darkTheme, lightTheme } from './theme';
 import Router from './routes/Router';
 import { QueryClient, QueryClientProvider } from "react-query";
 import GlobalStyle from './GlobalStyle';
-import { useState } from 'react';
-import { RecoilRoot, atom, useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { isDarkModeState } from './atoms';
 
 function App() {
-  const isDarkMode = useRecoilValue(isDarkModeState)
   return (
     <>
       <QueryClientProvider client={new QueryClient()}>
-        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <ThemeProvider theme={useRecoilValue(isDarkModeState) ? darkTheme : lightTheme}>
           <GlobalStyle />
           <Router />
         </ThemeProvider>
