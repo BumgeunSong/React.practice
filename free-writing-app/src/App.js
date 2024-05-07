eaa4583cf9009983b3fa1d9913a9b40128616e25
BumgeunSong <isp1195@gmail.com>
2024-05-07 10:44:42 +0900

    Material UI 텍스트 필드 가져다 쓰기

--- free-writing-app/src/App.js
+++ free-writing-app/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { TextField } from '@mui/material';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("")
   return (
     <div className="App">
-      
+      <TextField value={text} label="텍스트를 입력해주세요" onChange={(event) => {
+        setText(event.target.value)
+      }} />
+
+      <h3>Your Text:</h3>
+      <p>{text}</p>
     </div>
   );
 }
