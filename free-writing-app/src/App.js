e81b97b45516665b439e05dda161e18a9ad06640
BumgeunSong <isp1195@gmail.com>
2024-05-12 16:27:01 +0900

    글자수 세기 기능 추가

--- free-writing-app/src/App.js
+++ free-writing-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <TextField value={text} label="텍스트를 입력해주세요" onChange={(event) => {
         setText(event.target.value)
       }} />
-
+      <h2>글자수: {text.trim().length}</h2>
       <h3>Your Text:</h3>
       <p>{text}</p>
     </div>
